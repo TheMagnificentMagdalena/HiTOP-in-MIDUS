40d697990c5101908f911781084b3a56273d2145
Magdalena J March <march341@umn.edu>
2025-08-25 14:21:51 -0500

    Add files via upload

---
+++ Data Preperation/submit_imputation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l        
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=5
+#SBATCH --mem=64g
+#SBATCH --tmp=10g
+#SBATCH --mail-type=BEGIN,END,FAIL  
+#SBATCH --mail-user=march341@umn.edu
+#SBATCH --job-name=imputation_job
+#SBATCH --output=imputation_output_%j.txt
+
+cd /users/2/march341/multiple_imputation
+
+module load R/4.4.2-openblas-rocky8
+
+Rscript msi_imputation.R
